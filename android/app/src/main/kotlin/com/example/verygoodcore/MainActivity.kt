00f065c359e9bde17cac1e45441695724cb13e25
xmooncake <128915184+xmooncake@users.noreply.github.com>
2023-11-25 11:43:10 +0100

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.library_client
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_staging.dart
@@ -0,0 +1,6 @@
+import 'package:library_client/app/app.dart';
+import 'package:library_client/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
