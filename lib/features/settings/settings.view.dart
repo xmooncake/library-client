0745adbddf27b2afe7085795a87e83db87f5890d
Kiemonek <ytoconner@gmail.com>
2023-12-02 19:56:11 +0100

    Insert base layout for settings view

--- lib/features/settings/settings.view.dart
+++ lib/features/settings/settings.view.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 class SettingsView extends StatelessWidget {
@@ -5,8 +6,49 @@ class SettingsView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Settings'),
+    return SafeArea(
+      minimum: const EdgeInsets.all(100),
+      child: Card(
+        child: Row(
+          children: [
+            Flexible(
+              child: Padding(
+                padding: const EdgeInsets.all(25.0),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Image.network(
+                      'https://lh3.googleusercontent.com/8CUfRL089ZpHqozoSNvHMLQzxZHvQPtvQNiQDpaLFDDwFOKPtBz_mCTVX-teyMfN1ZV7p3Y4yIy3ATZrGzfPJI9Af2H4NEBovtcN4A',
+                    ),
+                    //TODO: Name from server
+                    const Text('Siusiusiu'),
+                    ListTile(
+                      title: const Text('Account Information'),
+                      onTap: () {},
+                    ),
+                    ListTile(
+                      title: const Text('Address Information'),
+                      onTap: () {},
+                    ),
+                    ListTile(
+                      title: const Text('Change Password'),
+                      onTap: () {},
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            const Flexible(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text('Siaiaiaiaia'),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
